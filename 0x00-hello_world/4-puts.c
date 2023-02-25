16acb1400c32721867200b003d7ee3857a092a68
Akojiflash <akojiflash2000@gmail.com>
2023-02-25 03:03:58 +0100

    Program is like building

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/**
+ * The main function prints a message using the puts function and returns 0.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
